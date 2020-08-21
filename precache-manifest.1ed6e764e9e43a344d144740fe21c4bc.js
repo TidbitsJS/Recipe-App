2136b93794f51bd7a37981bba215c63a0e625067
sujata <gunalesujata@gmail.com>
2020-08-21 23:28:23 +0530

    Updates

---
+++ precache-manifest.1ed6e764e9e43a344d144740fe21c4bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955fc613e89ed35a45002e73406230b1",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "52ec59a896e74a99ba34",
+    "url": "/Recipe-App/static/css/main.c661a048.chunk.css"
+  },
+  {
+    "revision": "6b654fee9435cda96412",
+    "url": "/Recipe-App/static/js/2.9c44afdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-App/static/js/2.9c44afdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52ec59a896e74a99ba34",
+    "url": "/Recipe-App/static/js/main.a7d338f7.chunk.js"
+  },
+  {
+    "revision": "c035ec1896e8fb26f1a6",
+    "url": "/Recipe-App/static/js/runtime-main.5105d1b0.js"
+  },
+  {
+    "revision": "8daaaa380d7d9f498ea0e9b2aa2ee211",
+    "url": "/Recipe-App/static/media/food.8daaaa38.jpeg"
+  }
+]);
